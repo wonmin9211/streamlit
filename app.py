688fc7d6d59654a7b12e4866025a719d43c77f58
wonmin9211 <126741206+wonmin9211@users.noreply.github.com>
2023-05-16 13:04:54 +0900

    Create app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import streamlit as st
+st.checkbox('yes')
+st.button('click')
+st.radio('Pick your gender', ['Male', 'Female'])
+st.selectbox('Pick your gender', ['Male', 'Female'])
+st.multiselect('choose a planet', ['Jupiter', 'Mars', 'neptune'])
+st.select_slider('Pick a mark', ['Bad', 'Good', 'Excellent'])
+st.slider('Pick a number', 0, 50)
