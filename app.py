f7bb2a94da85c50ebac4afdcab2a83a909ab28a7
wonmin9211 <126741206+wonmin9211@users.noreply.github.com>
2023-05-16 12:23:42 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
 import streamlit as st
-import matplotlib.pyplot as plt
-import numpy as np
 
-rand=np.random.normal(1,2,size=20)
-fig, ax = plt.subplots()
-ax.hist(rand,bins=15)
-st.pyplot(fig)
+st.title("this is the app title")
+st.markdown("this is the markdown")
+st.header("this is the header")
+st.subheader("this is the subheader")
+st.caption("this is the caption")
+st.code("x=2021")
+st.latex(r'''
+a + a r ^1+ a r^2 + a r^3 ''')
