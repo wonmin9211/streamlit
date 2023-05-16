9044cf30b602928560fda3b8ea017736b9c2d8b9
wonmin9211 <126741206+wonmin9211@users.noreply.github.com>
2023-05-16 13:18:46 +0900

    Update chaeyoung.py

--- chaeyoung.py
+++ chaeyoung.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
 st.title("챙용잉의 유튜브")
+st.markdown("https://www.youtube.com/@coddyddld0420")
 st.markdown("동아대학교 컴퓨터공학과 22학번 1짱 이채영")
 st.header("챙용잉의 다이어트")
 st.subheader("폴댄스 영상도 재밌어요")
